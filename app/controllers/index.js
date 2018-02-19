3ce3b9df93f5e9178061db8c561d05e5f9374644
collo <qollomcbizzy64@gmail.com>
2018-02-19 21:27:41 +0300

    added the index.js for the controller part

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,12 +15,14 @@ export default Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
 
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
+      const newInvitation = this.store.createRecord('invitation', { email });
 
-      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then(response => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+
     }
   }
 
-}); 
+});
